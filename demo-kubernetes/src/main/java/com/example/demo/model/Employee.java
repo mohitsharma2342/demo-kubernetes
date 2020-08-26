fdf86fa16fed3d757ef03e8716474b5f70dbee4c
mohitcse8@gmail.com <mohitcse8@gmail.com>
2020-08-26 12:33:16 +0530

    First Commit

---
+++ demo-kubernetes/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private Integer id;
+	@Column
+	private String name;
+	@Column
+	private String email;
+	@Column
+	private String mobileNo;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+		
+}
